b445e74a48e8b7a87360fb7d75f571a32a9cfbd6
unknown <rydzigor0232@gmail.com>
2026-01-13 12:08:53 +0100

    nie dziala

--- app/src/main/java/com/example/listatokarek/MainActivity.java
+++ app/src/main/java/com/example/listatokarek/MainActivity.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.ListView;
 import android.widget.RadioGroup;
@@ -24,6 +25,11 @@ import java.util.ArrayList;
 import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
+    String wybranaMarka;
+    String wybranyModel;
+    int wybranyRocznik;
+    boolean wybranyWlasciciel;
+    int wybranyStan;
 
     public class Samochod{
         String marka;
@@ -31,6 +37,25 @@ public class MainActivity extends AppCompatActivity {
         int rocznik;
         boolean wlasciciel;
         int stan;
+
+        public Samochod(String marka, String model, int rocznik, boolean wlasciciel, int stan) {
+            this.marka = marka;
+            this.model = model;
+            this.rocznik = rocznik;
+            this.wlasciciel = wlasciciel;
+            this.stan = stan;
+        }
+
+        @Override
+        public String toString() {
+            return "Samochod{" +
+                    "marka='" + marka + '\'' +
+                    ", model='" + model + '\'' +
+                    ", rocznik=" + rocznik +
+                    ", wlasciciel=" + wlasciciel +
+                    ", stan=" + stan +
+                    '}';
+        }
     }
 
     @Override
@@ -44,10 +69,11 @@ public class MainActivity extends AppCompatActivity {
         CheckBox wlasciciel = findViewById(R.id.wlasciciel);
         RadioGroup historia = findViewById(R.id.historia);
         ListView lista = findViewById(R.id.lista);
+        Button dodaj = findViewById(R.id.dodaj);
         ArrayList<Samochod> samochodyLista = new ArrayList<>();
         rocznik.setMax(Calendar.getInstance().get(Calendar.YEAR));
 
-        ArrayAdapter<Samochod> arrayAdapter = new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_list_item_1);
+        ArrayAdapter<Samochod> arrayAdapter = new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_list_item_1, samochodyLista);
         lista.setAdapter(arrayAdapter);
 
         ArrayList<String> modeleToyota = new ArrayList<>();
@@ -72,7 +98,7 @@ public class MainActivity extends AppCompatActivity {
         marka.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-                String wybranaMarka = adapterView.getSelectedItem().toString();
+                wybranaMarka = adapterView.getSelectedItem().toString();
                 ArrayAdapter<String> adapterModele = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_list_item_1, modele[i]);
                 model.setAdapter(adapterModele);
             }
@@ -80,6 +106,57 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onNothingSelected(AdapterView<?> adapterView) {
 
+            }
+        });
+
+        model.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                wybranyModel = adapterView.getSelectedItem().toString();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+        rocznik.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                wybranyRocznik = i;
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        wlasciciel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (view.isActivated()){
+                    wybranyWlasciciel = true;
+                }
+                else{
+                    wybranyWlasciciel = false;
+                }
+            }
+        });
+
+        wybranyStan = historia.getCheckedRadioButtonId();
+
+        dodaj.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                samochodyLista.add(new Samochod(wybranaMarka, wybranyModel, wybranyRocznik, wybranyWlasciciel, wybranyStan));
+                arrayAdapter.notifyDataSetChanged();
             }
         });
 
